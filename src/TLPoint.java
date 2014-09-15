1150453c14af56c5abc713f942f58b995e37413a
JonasTarnowetzki <jjt739@mail.usask.ca>
2014-09-15 08:58:18 -0600

    Added basic point class so I can figure out how to git on Eclipse. <_>

---
+++ src/TLPoint.java
@@ -0,0 +1,11 @@
+import java.awt.Point;
+
+
+public class TLPoint extends Point {
+
+	/**
+	 *  I'm not sure what this is used for, or if I need to use it.
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
